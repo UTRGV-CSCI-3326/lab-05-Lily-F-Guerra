f32fb203f5b5de3e399934cb3142d183d4cdbf8e
Lily F Guerra <115589260+Lily-F-Guerra@users.noreply.github.com>
2025-01-31 02:58:19 +0000

    First commit!

--- src/Input.java
+++ src/Input.java
@@ -1,1 +1,20 @@
+import java.util.Scanner;
 
+public class Input{
+    public static void main(String[] stringy){
+        Scanner fetch = new Scanner(System.in);
+        System.out.println("Please enter your name: ");
+        String name = fetch.nextLine();
+
+        System.out.println("Please enter your age: ");
+        int age = fetch.nextInt();
+
+        System.out.println("Please enter your weight: ");
+        double weight = fetch.nextDouble();
+
+        System.out.println("Have you ever smoked? Please enter true or false: ");
+        boolean smoker = fetch.nextBoolean();
+
+        System.out.println("Name: " + name + "   Age: " + age + "   Weight: " + weight + "   Smoker: " + smoker);
+    }
+}
